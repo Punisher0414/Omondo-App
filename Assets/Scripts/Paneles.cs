dc62dfc46976391f5c8e0eda71a03b724786c184
xgiannlinex <GiannaDigital@gmail.com>
2021-09-14 19:43:09 -0500

    Status y botones.
    Implementé los botones y el interfaz de status.

---
+++ Assets/Scripts/Paneles.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paneles : MonoBehaviour
+{
+	public GameObject panel;
+	public GameObject pActive;
+
+	public void Cerrar(){
+   	panel.SetActive(false);
+   }
+
+	public void Abrir(){
+   	panel.SetActive(true);
+   }
+
+   public void MsgActive(){
+   	StartCoroutine(PopUpMsg());
+   }
+
+	IEnumerator PopUpMsg(){
+		if (pActive.activeSelf){
+			pActive.SetActive(false);
+		}
+			panel.SetActive(true);
+			yield return new WaitForSeconds(2.5f);
+			panel.SetActive(false);
+		
+	}
+
+}
