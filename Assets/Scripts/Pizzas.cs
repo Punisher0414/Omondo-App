44835e1f31ef5ae3cd6a94bb6a8ff4ccfef09fdf
xgiannlinex <GiannaDigital@gmail.com>
2021-10-21 19:10:27 -0500

    All pizzas

--- Assets/Scripts/Pizzas.cs
+++ Assets/Scripts/Pizzas.cs
@@ -69,11 +69,10 @@ namespace Pizzas
 
    	}
 
-   	public class Vegetariana: Pizza
+   	public class Presumida: Pizza
    	{
-   		//Constructor.
-   		public Vegetariana(int quant):
-   			base("Pizza Vegetariana", 30000, "Base de tomate, queso, champiñón, maíz y piña", quant){
+   		public Presumida(int quant):
+   			base("Pizza La Presumidad", 32000, "Base de tomate, queso, queso de búfala, queso azul, queso parmesano, tocineta, albahaca y tomate perla.", quant){
           orderInstance._pizOrder.Add(this);
           Debug.Log(orderInstance._pizOrder.Count);
    			}
@@ -82,5 +81,122 @@ namespace Pizzas
 			   Debug.Log(Ingr);
 		  }
 
+   	}
+
+   	public class Estirada: Pizza
+   	{
+   		public Estirada(int quant):
+   			base("Pizza La Estirada", 33000, "Base de tomate, queso doble crema, queso bufala, queso azul, queso philadelphia y queso parmesano.", quant){
+          orderInstance._pizOrder.Add(this);
+          Debug.Log(orderInstance._pizOrder.Count);
+   			}
+
+		  protected override void PrintIngr(){
+			   Debug.Log(Ingr);
+		  }
+   	}
+
+   	public class Conchuda: Pizza
+   	{
+   		public Conchuda(int quant):
+   			base("Pizza La Conchuda", 33000, "Base BBQ, queso, carne en BBQ de la casa, queso philadelphia y queso azul.", quant){
+          orderInstance._pizOrder.Add(this);
+          Debug.Log(orderInstance._pizOrder.Count);
+   			}
+
+		  protected override void PrintIngr(){
+			   Debug.Log(Ingr);
+		  }
+   	}
+
+   	public class Chismosa: Pizza
+   	{
+   		public Chismosa(int quant):
+   			base("Pizza La Chismosa", 33000, "Base de tomate, queso, chorizo y chicharron caramelizados, madurito, maicitos y aguacate.", quant){
+          orderInstance._pizOrder.Add(this);
+          Debug.Log(orderInstance._pizOrder.Count);
+   			}
+
+		  protected override void PrintIngr(){
+			   Debug.Log(Ingr);
+		  }
+   	}
+
+   	public class Bichota: Pizza
+   	{
+   		public Bichota(int quant):
+   			base("Pizza La Bichota", 35000, "Base de tomate, queso, albahaca fresca, tomate perla, queso philadelphia y burrata.", quant){
+          orderInstance._pizOrder.Add(this);
+          Debug.Log(orderInstance._pizOrder.Count);
+   			}
+
+		  protected override void PrintIngr(){
+			   Debug.Log(Ingr);
+		  }
+   	}
+
+   	public class Chocolate: Pizza
+   	{
+   		public Chocolate(int quant):
+   			base("Pizza de Chocolate", 28000, "Base de chocolate, queso, lecherita, bocadillo y fresas.", quant){
+          orderInstance._pizOrder.Add(this);
+          Debug.Log(orderInstance._pizOrder.Count);
+   			}
+
+		  protected override void PrintIngr(){
+			   Debug.Log(Ingr);
+		  }
+   	}
+
+   	public class Carnuda: Pizza
+   	{
+   		public Carnuda(int quant):
+   			base("Pizza La Carnuda", 32000, "Base de tomate, queso, jamon, salami, peperoni, cabano y tocineta.", quant){
+          orderInstance._pizOrder.Add(this);
+          Debug.Log(orderInstance._pizOrder.Count);
+   			}
+
+		  protected override void PrintIngr(){
+			   Debug.Log(Ingr);
+		  }
+   	}
+
+   	public class Hierbabuena: Pizza
+   	{
+   		public Hierbabuena(int quant):
+   			base("Limonada de Hierbabuena", 6500, "", quant){
+          orderInstance._pizOrder.Add(this);
+          Debug.Log(orderInstance._pizOrder.Count);
+   			}
+
+		  protected override void PrintIngr(){
+			   Debug.Log(Ingr);
+		  }
+   	}
+
+   	public class Coco: Pizza
+   	{
+   		public Coco(int quant):
+   			base("Limonada de Coco", 8000, "", quant){
+          orderInstance._pizOrder.Add(this);
+          Debug.Log(orderInstance._pizOrder.Count);
+   			}
+
+		  protected override void PrintIngr(){
+			   Debug.Log(Ingr);
+		  }
+   	}
+
+   	public class Cereza: Pizza
+   	{
+   		public Cereza(int quant):
+   			base("Limonada de Cereza", 6500, "", quant){
+          orderInstance._pizOrder.Add(this);
+          Debug.Log(orderInstance._pizOrder.Count);
+   			}
+
+		  protected override void PrintIngr(){
+			   Debug.Log(Ingr);
+		  }
    	}
 }
